019d1b0e36392828b214283305688992fe905e70
andreivercosa <andreivercosa@gmail.com>
2019-03-21 21:39:26 -0400

    v1

---
+++ ConsumoListaApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsumoListaApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Dictionary<string, string> conteudo = new Dictionary<string, string>();
+            conteudo.Add("Professor", "Ramon");
+            conteudo.Add("Aluno", "Michael");
+            conteudo.Add("Mito", "Lula");
+            conteudo.Add("Oraculo", "Dilma");
+
+            var listaItens = conteudo.Keys.ToList();
+            ArrayList lista = new ArrayList();
+            for (int i=0; i < listaItens.Count; i++)
+            {
+                if (listaItens[i].Contains("Oraculo"))
+                {
+                    lista.Add(listaItens[i]);
+                }
+
+            }
+            listaMunicipio.ItemsSource = lista;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
